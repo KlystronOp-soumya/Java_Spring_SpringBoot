636645e904689c4a5fc650d0d3f55c7c8c9667cc
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-04-10 02:17:51 +0530

    Adds instruction for Batch Launch

--- linkedin-batch-01-04/src/main/java/com/linkedin/batch/MyBatchRunner.java
+++ linkedin-batch-01-04/src/main/java/com/linkedin/batch/MyBatchRunner.java
@@ -1,5 +1,13 @@
 package com.linkedin.batch;
 
+/*
+ * To run this commandLineRunner 
+ * run the golas: mvn clean compile package
+ * navigate to the target folder then 
+ * type: java jar "jar name" MyBatchRunner **provide the batch parameters if required
+ * 
+ * 
+ * */
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,33 +21,32 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyBatchRunner implements CommandLineRunner{
+public class MyBatchRunner implements CommandLineRunner {
 
 	@Autowired
 	JobLauncher jobLauncher;
 	@Autowired
-	Job job ;
+	Job job;
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		Map<String, JobParameter> maps = new HashMap<>();
-        maps.put("time", new JobParameter(System.currentTimeMillis()));
-        JobParameters parameters = new JobParameters(maps);
-        JobExecution jobExecution = jobLauncher.run(job, parameters);//here the job triggers the SpringBatchJobConfig file.
-        //the inbuilt job config is disabled 
-        
-       
-        System.out.println("JobExecution: " + jobExecution.getStatus());
-     
+		maps.put("time", new JobParameter(System.currentTimeMillis()));
+		JobParameters parameters = new JobParameters(maps);
+		JobExecution jobExecution = jobLauncher.run(job, parameters);// here the job triggers the SpringBatchJobConfig
+																		// file.
+		// the inbuilt job config is disabled
 
-        System.out.println("Batch is Running...");
-        while (jobExecution.isRunning()) {
-            System.out.println("...");
-        }
+		System.out.println("JobExecution: " + jobExecution.getStatus());
 
-        //return jobExecution.getStatus();
-		
+		System.out.println("Batch is Running...");
+		while (jobExecution.isRunning()) {
+			System.out.println("...");
+		}
+
+		// return jobExecution.getStatus();
+
 	}
-	
 
 }
