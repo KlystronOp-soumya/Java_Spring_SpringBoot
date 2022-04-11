42f0088adc7bec26a97e54781a323bc6eab431d8
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-04-12 02:41:55 +0530

    H2 Database Config

--- src/main/java/com/demo/batch/config/DataSourceConfig.java
+++ src/main/java/com/demo/batch/config/DataSourceConfig.java
@@ -2,13 +2,15 @@ package com.demo.batch.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
@@ -32,15 +34,19 @@ public class DataSourceConfig {
 	private String pwdMySql;
 
 	@NonNull
+	@Value(value = "${h2.url}")
 	private String dbUrlH2;
 	@NonNull
+	@Value(value = "${h2.driver}")
 	private String dbDriverH2;
 	@NonNull
+	@Value(value = "${h2.user}")
 	private String userH2;
 	@Nullable
+	@Value(value = "${h2.password}")
 	private String pwdH2;
 
-	@Bean
+	@Bean(name = "mysqlDS")
 	@Primary
 	public DataSource dataSourceMySql() {
 		this.dataSourceBuilder = DataSourceBuilder.create();
@@ -51,8 +57,7 @@ public class DataSourceConfig {
 		return dataSourceBuilder.build();
 	}
 
-	@Bean
-	@Qualifier("dataSourceH2")
+	@Bean(name = "dh2DS")
 	public DataSource dataSourceH2() {
 		this.dataSourceBuilder = DataSourceBuilder.create();
 		this.dataSourceBuilder.driverClassName(this.dbDriverH2);
@@ -60,5 +65,19 @@ public class DataSourceConfig {
 		this.dataSourceBuilder.username(this.userH2);
 		this.dataSourceBuilder.password(this.pwdH2);
 		return dataSourceBuilder.build();
+	}
+
+	@Bean
+	public DataSourceInitializer h2DatabasePopulator() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("org/springframework/batch/core/schema-h2.sql"));
+		populator.setContinueOnError(true);
+		populator.setIgnoreFailedDrops(true);
+
+		DataSourceInitializer initializer = new DataSourceInitializer();
+		initializer.setDatabasePopulator(populator);
+		initializer.setDataSource(dataSourceH2());
+
+		return initializer;
 	}
 }
