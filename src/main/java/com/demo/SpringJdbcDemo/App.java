e6803f2161c2b38ef21c4b671868e4518b6223af
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-04-13 02:10:33 +0530

    initial commit for Spirng JDBC Demo

---
+++ src/main/java/com/demo/SpringJdbcDemo/App.java
@@ -0,0 +1,16 @@
+package com.demo.SpringJdbcDemo;
+
+/**
+ * Main class for the Spring JDBC ultimate Demo
+ *
+ * @author SOUMYADEEP PAUL
+ *
+ * @category Demo
+ *
+ * @version 1.0.0
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/main/java/com/demo/SpringJdbcDemo/Configuration/SpringJdbcDemoConfig.java
@@ -0,0 +1,8 @@
+package com.demo.SpringJdbcDemo.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringJdbcDemoConfig {
+
+}
