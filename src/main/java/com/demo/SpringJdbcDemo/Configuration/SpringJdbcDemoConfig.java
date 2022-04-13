568653d966388bacf6fa05edb2b913de2ed0c2f9
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-04-14 02:50:19 +0530

    Annotates the class for configuration

--- src/main/java/com/demo/SpringJdbcDemo/Configuration/SpringJdbcDemoConfig.java
+++ src/main/java/com/demo/SpringJdbcDemo/Configuration/SpringJdbcDemoConfig.java
@@ -1,8 +1,14 @@
 package com.demo.SpringJdbcDemo.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.demo.SpringJdbcDemo.service.TestService;
 
 @Configuration
 public class SpringJdbcDemoConfig {
-
+	@Bean("testServiceBean")
+	public TestService testService() {
+		return new TestService();
+	}
 }
