7383b3157e56321f01c6eae7ddae087ade6fe738
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-04-21 03:00:52 +0530

    Adds and implements DAO class

--- BatchDemo/src/main/java/com/demo/batch/BatchDemo/DAO/impl/AgentBonusDAOImpl.java
+++ BatchDemo/src/main/java/com/demo/batch/BatchDemo/DAO/impl/AgentBonusDAOImpl.java
@@ -1,7 +1,72 @@
 package com.demo.batch.BatchDemo.DAO.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import com.demo.batch.BatchDemo.DAO.intf.AgentBonusDAO;
+import com.demo.batch.BatchDemo.entity.AgentBnsQualifEntity;
 
 public class AgentBonusDAOImpl implements AgentBonusDAO {
+
+	private transient DataSource dataSource;
+
+	private Connection connection;
+
+	private ResultSet rs;
+
+	@Override
+	public List<AgentBnsQualifEntity> getAgentQualifData() {
+		// TODO Auto-generated method stub
+		List<AgentBnsQualifEntity> qualifAgtList = new ArrayList<>();
+		this.connection = null;
+		String query = "SELECT AGT_ID,NAME,DESIGNATION,LOB,BONUS,BONUS_PCT,BONUS_PAYOUT,CALDAY,CALMONTH,CALYEAR FROM agent_bonus_qualif";
+		try {
+			this.connection = this.dataSource.getConnection();
+			PreparedStatement pstmt = this.connection.prepareStatement(query);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				// Create the POJO
+				AgentBnsQualifEntity agentBnsQualifEntity = new AgentBnsQualifEntity();
+				agentBnsQualifEntity.setAgtId(rs.getString("AGT_ID"));
+				agentBnsQualifEntity.setName(rs.getString("NAME"));
+				agentBnsQualifEntity.setDesignation(rs.getString("DESIGNATION"));
+				agentBnsQualifEntity.setLOB(rs.getString("LOB"));
+				agentBnsQualifEntity.setBonusAmt(rs.getBigDecimal("BONUS"));
+				agentBnsQualifEntity.setBonusPerct(rs.getBigDecimal("BONUS_PCT"));
+				agentBnsQualifEntity.setBonusPayout(rs.getBigDecimal("BONUS_PAYOUT"));
+				agentBnsQualifEntity.setCalDay(rs.getInt("CALDAY"));
+				agentBnsQualifEntity.setCalMonth(rs.getInt("CALMONTH"));
+				agentBnsQualifEntity.setCalYear(rs.getInt("CALYEAR"));
+
+				qualifAgtList.add(agentBnsQualifEntity);
+
+			}
+			if (qualifAgtList == null || qualifAgtList.isEmpty())
+				throw new NullPointerException("The qualified Agent List Can not be empty");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return qualifAgtList;
+	}
+
+	@Override
+	public void setDataSource(DataSource dataSource) {
+		// TODO Auto-generated method stub
+		this.dataSource = dataSource;
+
+	}
+
+	@Override
+	public void saveQualifAgentList(List<AgentBnsQualifEntity> agentBnsQualifList) {
+		// TODO Auto-generated method stub
+		// to be used in the Writer
+	}
 
 }
--- BatchDemo/src/main/java/com/demo/batch/BatchDemo/DAO/intf/AgentBonusDAO.java
+++ BatchDemo/src/main/java/com/demo/batch/BatchDemo/DAO/intf/AgentBonusDAO.java
@@ -1,6 +1,16 @@
 package com.demo.batch.BatchDemo.DAO.intf;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.demo.batch.BatchDemo.entity.AgentBnsQualifEntity;
+
 public interface AgentBonusDAO {
 
-	//
+	void setDataSource(DataSource dataSource);
+
+	List<AgentBnsQualifEntity> getAgentQualifData();
+
+	void saveQualifAgentList(List<AgentBnsQualifEntity> agentBnsQualifList);
 }
