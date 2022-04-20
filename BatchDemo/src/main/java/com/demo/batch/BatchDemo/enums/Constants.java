00b64c316adde6755e909a2a7df5bf4088752c76
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-04-21 03:01:45 +0530

    Adds excel path

--- BatchDemo/src/main/java/com/demo/batch/BatchDemo/enums/Constants.java
+++ BatchDemo/src/main/java/com/demo/batch/BatchDemo/enums/Constants.java
@@ -2,7 +2,8 @@ package com.demo.batch.BatchDemo.enums;
 
 public enum Constants {
 
-	SPRING_BATCH_CONFIG_PATH("spring/batch/jobs/batch-job-config.xml");
+	SPRING_BATCH_CONFIG_PATH("spring/batch/jobs/batch-job-config.xml"),
+	EXCEL_REPORT_PATH("./results/AGENT_BONUS_DATA.xls");
 
 	public String value;
 
