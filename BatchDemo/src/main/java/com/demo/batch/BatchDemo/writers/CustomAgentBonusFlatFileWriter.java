6bb73d38f18393af404fc4c8e2aac95c5d35a235
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-05-06 01:21:34 +0530

    Implements classes for Flat File writing

--- BatchDemo/src/main/java/com/demo/batch/BatchDemo/writers/CustomAgentBonusFlatFileWriter.java
+++ BatchDemo/src/main/java/com/demo/batch/BatchDemo/writers/CustomAgentBonusFlatFileWriter.java
@@ -1,19 +1,37 @@
 package com.demo.batch.BatchDemo.writers;
 
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.core.io.Resource;
 
 import com.demo.batch.BatchDemo.entity.AgentBnsQualifEntity;
 
 public class CustomAgentBonusFlatFileWriter extends FlatFileItemWriter<AgentBnsQualifEntity> {
 
 	/*
-	 * TODO: Override setResources method TODO: Override setLineAggregator method
-	 * TODO: Override shouldDeleteIfExists method
 	 * 
-	 * Add this as the tasklet into XML
+	 * Add this as the tasklet writer into XML
 	 * 
 	 * Create a seperate reader for this copy job; add that as well into XML
 	 * 
 	 */
+	private transient Resource resource;
+	private transient LineAggregator<AgentBnsQualifEntity> lineAggregator;
+	private transient boolean shouldDeleteIfExists;
+
+	@Override
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+
+	@Override
+	public void setLineAggregator(LineAggregator<AgentBnsQualifEntity> lineAggregator) {// UtilityClass Obj
+		this.lineAggregator = lineAggregator;
+	}
+
+	@Override
+	public void setShouldDeleteIfExists(boolean shouldDeleteIfExists) {
+		this.shouldDeleteIfExists = shouldDeleteIfExists;
+	}
 
 }
--- BatchDemo/src/main/java/com/demo/batch/Util/CustomLineAggregatorUtil.java
+++ BatchDemo/src/main/java/com/demo/batch/Util/CustomLineAggregatorUtil.java
@@ -1,15 +1,20 @@
 package com.demo.batch.Util;
 
+import org.springframework.batch.item.file.transform.FieldExtractor;
 import org.springframework.batch.item.file.transform.FormatterLineAggregator;
 
 import com.demo.batch.BatchDemo.entity.AgentBnsQualifEntity;
 
 public class CustomLineAggregatorUtil extends FormatterLineAggregator<AgentBnsQualifEntity> {
-	/*
-	 * 
-	 * override setFormat override override setFieldExtractor
-	 * 
-	 * Add this as well in the XML as bean and inject into the FlatFileItemReader
-	 * 
-	 */
+	private String format = null;
+	private transient FieldExtractor<AgentBnsQualifEntity> fieldExtractor;
+
+	public void setFormat(final String format) {
+		this.format = format;
+	}
+
+	@Override
+	public void setFieldExtractor(FieldExtractor<AgentBnsQualifEntity> fieldExtractor) {
+		this.fieldExtractor = fieldExtractor;
+	}
 }
