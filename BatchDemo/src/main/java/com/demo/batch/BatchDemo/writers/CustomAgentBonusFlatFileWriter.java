dd0e403d872b371c01ac870b6997aa1681e697bd
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-05-06 14:45:40 +0530

    Adds Logger

--- BatchDemo/src/main/java/com/demo/batch/BatchDemo/writers/CustomAgentBonusFlatFileWriter.java
+++ BatchDemo/src/main/java/com/demo/batch/BatchDemo/writers/CustomAgentBonusFlatFileWriter.java
@@ -1,5 +1,6 @@
 package com.demo.batch.BatchDemo.writers;
 
+import org.apache.log4j.Logger;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.transform.LineAggregator;
 import org.springframework.core.io.Resource;
@@ -8,6 +9,8 @@ import com.demo.batch.BatchDemo.entity.AgentBnsQualifEntity;
 
 public class CustomAgentBonusFlatFileWriter extends FlatFileItemWriter<AgentBnsQualifEntity> {
 
+	/* logger */
+	private static final Logger LOGGER = Logger.getLogger(CustomAgentBonusFlatFileWriter.class);
 	/*
 	 * 
 	 * Add this as the tasklet writer into XML
@@ -21,11 +24,13 @@ public class CustomAgentBonusFlatFileWriter extends FlatFileItemWriter<AgentBnsQ
 
 	@Override
 	public void setResource(Resource resource) {
+		LOGGER.info("Set resource for Flat File Writing");
 		this.resource = resource;
 	}
 
 	@Override
 	public void setLineAggregator(LineAggregator<AgentBnsQualifEntity> lineAggregator) {// UtilityClass Obj
+		LOGGER.info("Set LineAggregator for Flat File Writing");
 		this.lineAggregator = lineAggregator;
 	}
 
