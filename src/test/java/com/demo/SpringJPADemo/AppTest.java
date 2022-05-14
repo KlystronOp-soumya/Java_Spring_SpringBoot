54bb4e6a65ef57be3e06425afafc8c20cbb84535
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-05-15 01:28:53 +0530

    Adds unit test methods

--- src/test/java/com/demo/SpringJPADemo/AppTest.java
+++ src/test/java/com/demo/SpringJPADemo/AppTest.java
@@ -1,38 +1,35 @@
 package com.demo.SpringJPADemo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Before
+	public void initialize() {
+		// method to initialize db
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void dummy_test() {
+		assertTrue("This test will be failed", false);
+	}
+
+	@Test(expected = SQLException.class)
+	public void unseccessful_DBConnectivity_test() {
+
+	}
+
+	@Test
+	public void doCheckDBConnection_test() {
+
+	}
+
 }
