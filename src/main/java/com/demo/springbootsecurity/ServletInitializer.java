85eaf329cd3ccca1ae1395e7c1886d99d2537de6
KlystronOp-soumya <soumyadeep62@gmail.com>
2022-11-18 21:51:07 +0530

    Adds application runner and Servlet config class

---
+++ src/main/java/com/demo/springbootsecurity/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.demo.springbootsecurity;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootBasicSecurityApplication.class);
+	}
+
+}
