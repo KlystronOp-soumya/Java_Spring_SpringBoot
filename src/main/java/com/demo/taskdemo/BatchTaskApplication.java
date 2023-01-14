8c53072274445f3ccdda7e2d26d09fa12a38a7dd
KlystronOp-soumya <soumyadeep62@gmail.com>
2023-01-15 01:11:24 +0530

    removes annotation for disabling task auto configuration

--- src/main/java/com/demo/taskdemo/BatchTaskApplication.java
+++ src/main/java/com/demo/taskdemo/BatchTaskApplication.java
@@ -1,12 +1,10 @@
 package com.demo.taskdemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.task.configuration.SimpleTaskAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = SimpleTaskAutoConfiguration.class)
+//@EnableAutoConfiguration(exclude = SimpleTaskAutoConfiguration.class)
 public class BatchTaskApplication {
 
 	public static void main(String[] args) {
