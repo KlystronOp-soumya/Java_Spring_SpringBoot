cb73b094a1e3d63505bfda9f4b6b6a94531f314a
KlystronOp-soumya <soumyadeep62@gmail.com>
2023-11-28 01:55:16 +0530

    Adds Calendar to check the time

--- src/main/java/com/demo/springbatch/steps/DispHelloWorld.java
+++ src/main/java/com/demo/springbatch/steps/DispHelloWorld.java
@@ -1,5 +1,7 @@
 package com.demo.springbatch.steps;
 
+import java.util.Calendar;
+
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -9,7 +11,7 @@ public class DispHelloWorld implements Tasklet {
 
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World" + Calendar.getInstance());
 		
 		return RepeatStatus.FINISHED;
 	}
