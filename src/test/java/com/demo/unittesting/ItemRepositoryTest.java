4d31a022015f3f2221608964cf33de00125928e8
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-06-30 17:09:50 +0530

    Adds method to test repository findAll

--- src/test/java/com/demo/unittesting/ItemRepositoryTest.java
+++ src/test/java/com/demo/unittesting/ItemRepositoryTest.java
@@ -1,10 +1,31 @@
 package com.demo.unittesting;
 //Test class to test the ItemRepo
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.repository.config.BootstrapMode;
+
+import com.demo.unittesting.dao.ItemRepository;
+import com.demo.unittesting.entities.Item;
 
 // @RunWith(SpringRunner.class) -- this is not required with Junit 5
-@DataJpaTest
+@DataJpaTest(bootstrapMode = BootstrapMode.DEFAULT, showSql = true)
+//@TestPropertySource
 public class ItemRepositoryTest {
 
+	@Autowired
+	private transient ItemRepository itemRepository;
+
+	@Test
+	public void test_findAll() {
+		List<Item> items = itemRepository.findAll();
+
+		assertThat(items.size()).isEqualTo(4);
+
+	}
 }
