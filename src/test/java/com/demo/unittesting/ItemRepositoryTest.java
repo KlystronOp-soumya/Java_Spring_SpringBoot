894f81b108c33aee45f167c10d1dea06bf62f7db
KlystronOp-soumya <soumyadeep62@gmail.com>
2024-06-30 16:44:15 +0530

    adds unit test class to test the repositroy

---
+++ src/test/java/com/demo/unittesting/ItemRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.demo.unittesting;
+//Test class to test the ItemRepo
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+// @RunWith(SpringRunner.class) -- this is not required with Junit 5
+@DataJpaTest
+public class ItemRepositoryTest {
+
+}
