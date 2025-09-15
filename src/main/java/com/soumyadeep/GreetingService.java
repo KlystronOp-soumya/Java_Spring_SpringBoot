a9e4debf8120ddeb19372fcb85db78cdd0bdf3c2
KlystronOp-soumya <soumyadeep62@gmail.com>
2025-09-15 14:32:23 +0530

    initial commit

---
+++ src/main/java/com/soumyadeep/GreetingService.java
@@ -0,0 +1,10 @@
+package com.soumyadeep;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "Guten Tag " + name;
+    }
+}
